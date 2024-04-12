73ff13a01ae1ef9fc59e169f91a31da60b7750b9
Aprameya <82435060+aprameyap@users.noreply.github.com>
2024-04-12 19:13:21 +0530

    Update example.sh

--- example.sh
+++ example.sh
@@ -6,7 +6,7 @@ export TORCH_INSTALL=path/to/torch-2.2.0a0+81ea7a4+nv23.12-cp38-cp38-linux_aarch
 
 python3 -m pip install --upgrade pip;
 python3 -m pip install numpy==’1.26.1’;
-python3 -m pip install --no-cache $TORCH_INSTALL
+python3 -m pip install --no-cache $TORCH_INSTALL;
 
 git clone --recursive --branch <branch_name i.e. version> https://github.com/pytorch/vision
 cd vision
